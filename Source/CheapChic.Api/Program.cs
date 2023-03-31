c51de20e0a98db8160d71b890e75b29cfe6adac1
Anton Osadchii <roflandcom@gmail.com>
2023-03-31 22:28:50 +0300

    migrate db

--- Source/CheapChic.Api/Program.cs
+++ Source/CheapChic.Api/Program.cs
@@ -1,4 +1,6 @@
 using CheapChic.Api.Configuration;
+using CheapChic.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.ConfigureConfiguration();
@@ -7,4 +9,13 @@ builder.Services.ConfigureServices(builder.Configuration);
 var application = builder.Build();
 application.ConfigureApplication();
 
+InitializeDatabase(application);
+
 application.Run();
+
+
+static void InitializeDatabase(IApplicationBuilder app)
+{
+    using var scope = app.ApplicationServices.GetService<IServiceScopeFactory>()?.CreateScope();
+    scope?.ServiceProvider.GetRequiredService<CheapChicContext>().Database.Migrate();
+}
