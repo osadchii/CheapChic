d608e71de01fecc2821eea4c491b0195e5784e5b
a.osadchii <roflandcom@gmail.com>
2023-04-02 22:10:50 +0300

    metrics

--- Source/CheapChic.Infrastructure/Handlers/Telegram/Commands/HandleUpdate.cs
+++ Source/CheapChic.Infrastructure/Handlers/Telegram/Commands/HandleUpdate.cs
@@ -2,6 +2,7 @@ using CheapChic.Infrastructure.UpdateHandlers.CallbackQuery;
 using CheapChic.Infrastructure.UpdateHandlers.Message;
 using CheapChic.Infrastructure.UpdateHandlers.MyChatMember;
 using MediatR;
+using Prometheus;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
@@ -23,9 +24,13 @@ public static class HandleUpdate
 
     public class Handler : IRequestHandler<Command, Unit>
     {
+        private static readonly Counter ReceivedMessages = Metrics
+            .CreateCounter("received_messages", "Number of received messages.");
+
         private readonly ICallbackQueryHandler _callbackQueryHandler;
         private readonly IMessageHandler _messageHandler;
         private readonly IMyChatMemberHandler _myChatMemberHandler;
+
 
         public Handler(IMessageHandler messageHandler, ICallbackQueryHandler callbackQueryHandler,
             IMyChatMemberHandler myChatMemberHandler)
@@ -37,6 +42,7 @@ public static class HandleUpdate
 
         public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
         {
+            ReceivedMessages.Inc();
             var update = request.Update;
             switch (update.Type)
             {
